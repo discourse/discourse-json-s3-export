df1ec339df79cda3b004d6f8a486d8ef534d72c6
Samer Masry <samer.masry@gmail.com>
2017-06-15 09:32:07 -0700

    Using txn isolation and splitting out into smaller jobs

--- plugin.rb
+++ plugin.rb
@@ -7,47 +7,90 @@
 enabled_site_setting :json_s3_export_enabled
 
 after_initialize do
-  require 'active_record/core'
+  require_dependency 'jobs/base'
+
+  module ::Jobs
+    class ExportTableToS3 < Jobs::Base
+      BATCH_SIZE = 1000.freeze
+      BLACK_LISTED_COLUMNS = {
+        'users' => [
+          :encrypted_password, :reset_password_token,
+          :old_password, :password_salt
+        ]
+      }.freeze
+
+      sidekiq_options queue: 'low'
+
+      def execute(args)
+        ar_class = args[:class_name].constantize
+        start = args[:start] || 1
+        table_name = ar_class.table_name
+        ar_class.connection.transaction do
+          ar_class.connection.execute('SET TRANSACTION ISOLATION LEVEL SERIALIZABLE')
+          ar_class.all.find_in_batches(start: start, batch_size: BATCH_SIZE).with_index do |group, batch|
+            Tempfile.open(table_name) do |f|
+
+              Zlib::GzipWriter.open(f) do |gz|
+                group.each do |record|
+                  gz.puts record.attributes.except(*BLACK_LISTED_COLUMNS[table_name])
+                end
+              end
+
+              # Upload File to S3
+              upload_to_s3("#{table_name}/data-#{start}.gz")
+
+              # schedule next set
+              if group.size == BATCH_SIZE
+                Jobs.enqueue(:export_table_to_s3, class_name: ar_class.to_s,
+                                                  start: group.last[ar_class.primary_key] + 1)
+              end
+            end
+          end
+        end
+      end
+
+      private
+
+      def upload_to_s3(file_name)
+        obj = s3.bucket(SiteSetting.json_s3_export_bucket).object(file_name)
+        obj.upload_file(f, server_side_encryption: 'AES256')
+      end
+
+      def aws_client
+        Aws::S3::Client.new(
+          region: SiteSetting.json_s3_export_region,
+          access_key_id: SiteSetting.json_s3_export_access_key,
+          secret_access_key: SiteSetting.json_s3_export_secret_key
+        )
+      end
+
+      def s3
+        s3 = Aws::S3::Resource.new(client: aws_client)
+      end
+    end
+  end
+
+
   module ::Jobs
     class JsonS3Export < Jobs::Scheduled
-      BATCH_SIZE = 1000
+
       sidekiq_options queue: 'low'
+
+      CLASSES_FOR_EXPORT = [
+        BadgeGrouping, BadgeType, Badge, Category, CategoryFeaturedTopic,
+        CategoryFeaturedUser, CategoryGroup, CategoryTagGroup, CategoryTag,
+        CategoryUser, GroupMention, GroupUser, Group, PostActionType,
+        PostDetail, TagGroup, TagUser, TopTopic, TopicTag,
+        TopicUser, Topic, UserAction, UserBadge, UserVisit, User
+      ].freeze
 
       every 1.day
       def execute(args)
         return unless SiteSetting.json_s3_export_enabled
 
-        Discourse.enable_readonly_mode if SiteSetting.json_s3_export_readonly
-
-        ActiveRecord::Base.subclasses.each do |ar_class|
-          table_name = ar_class.table_name
-          begin
-            ar_class.all.find_in_batches(batch_size: BATCH_SIZE).with_index do |group, batch|
-              Tempfile.open(table_name) do |f|
-                Zlib::GzipWriter.open(f) do |gz|
-                  group.each do |record|
-                    gz.puts record.to_json
-                  end
-                end
-                client = Aws::S3::Client.new(
-                  region: SiteSetting.json_s3_export_region,
-                  access_key_id: SiteSetting.json_s3_export_access_key,
-                  secret_access_key: SiteSetting.json_s3_export_secret_key
-                )
-                s3 = Aws::S3::Resource.new(client: client)
-                file_name = "#{table_name}/data-#{batch}.gz"
-                obj = s3.bucket(SiteSetting.json_s3_export_bucket).object(file_name)
-                obj.upload_file(f)
-              end
-            end
-          rescue => e
-            # A few tables are not able to be uploaded
-            # caused by no primary key
-            Sidekiq.logger.error "Unable to upload #{ar_class.to_s}"
-          end
+        CLASSES_FOR_EXPORT.each do |ar_class|
+          Jobs.enqueue(:export_table_to_s3, class_name: ar_class.to_s)
         end
-      ensure
-        Discourse.disable_readonly_mode if SiteSetting.json_s3_export_readonly
       end
     end
   end
